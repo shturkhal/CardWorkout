7ec0fd4c9315f34a5246dd0760bf8cfc036beded
shturkhal <107053994+shturkhal@users.noreply.github.com>
2023-09-11 13:36:46 +0300

    add outlets for imageView and buttons

--- CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardSelectionVC.swift
@@ -9,21 +9,22 @@ import UIKit
 
 class CardSelectionVC: UIViewController {
 
+    //MARK: Outlets
+    
+    @IBOutlet var cardImageView: UIImageView!
+    @IBOutlet var stopButton: UIButton!
+    @IBOutlet var restartButton: UIButton!
+    @IBOutlet var rulesButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+    //MARK: Actions
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
